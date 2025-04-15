c68b188d78876051d23175ba83c5e7d38d62de51
isabella dutra <dutraisabella126@gmail.com>
2025-04-15 11:06:29 -0300

    meu projeto

--- src/app/meu-formulario/meu-formulario.component.ts
+++ src/app/meu-formulario/meu-formulario.component.ts
@@ -13,7 +13,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { ApiService } from '../services/api.service';
 
 interface User {
-  
+  id: typeof;
   name: string;
   email: string;
   password?: string;
